7967286d322df8ff85f065bd4bf886cd54579f81
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-03-26 15:56:43 -0400

    crud

--- W25W11TypedDataSets/MainWindow.xaml.cs
+++ W25W11TypedDataSets/MainWindow.xaml.cs
@@ -16,9 +16,86 @@ namespace W25W11TypedDataSets
     /// </summary>
     public partial class MainWindow : Window
     {
+        // table adapters
+        NorthwindDataSetTableAdapters.ProductsTableAdapter adpProducts = new NorthwindDataSetTableAdapters.ProductsTableAdapter();
+
+        // data tables
+        NorthwindDataSet.ProductsDataTable tblProducts = new NorthwindDataSet.ProductsDataTable();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            LoadProducts();
+        }
+
+        private void LoadProducts()
+        {
+            // Fill method
+            //adpProducts.Fill(tblProducts);
+
+            // Get method
+            tblProducts = adpProducts.GetProducts();
+
+            grdProducts.ItemsSource = tblProducts;
+        }
+
+        private void btnLoadAllProducts_Click(object sender, RoutedEventArgs e)
+        {
+            LoadProducts();
+        }
+
+        private void btnFind_Click(object sender, RoutedEventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var row = tblProducts.FindByProductID(id);
+
+            if (row != null)
+            {
+                txtName.Text = row.ProductName;
+                txtPrice.Text = row.UnitPrice.ToString();
+                txtQuantity.Text = row.UnitsInStock.ToString();
+            }
+            else
+            {
+                txtName.Text = txtPrice.Text = txtQuantity.Text = "";
+                MessageBox.Show("Invalid ID. Please try again");
+            }
+        }
+
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtName.Text;
+            decimal price = decimal.Parse(txtPrice.Text);
+            short quantity = short.Parse(txtQuantity.Text);
+
+            adpProducts.Insert(name, price, quantity);
+
+            LoadProducts();
+            MessageBox.Show("New product added");
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            string name = txtName.Text;
+            decimal price = decimal.Parse(txtPrice.Text);
+            short quantity = short.Parse(txtQuantity.Text);
+
+            adpProducts.Update(name, price, quantity, id);
+
+            LoadProducts();
+            MessageBox.Show("Product updated");
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+
+            adpProducts.Delete(id);
+
+            LoadProducts();
+            MessageBox.Show("Product deleted");
         }
     }
 }
